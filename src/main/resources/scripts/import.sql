b1137ce0f06bbc7422b36930f656909a4ceacc5e
Jan Dosedel <jan.dosedel@plus4u.net>
2020-03-14 19:53:30 +0100

    update import arts table records

--- src/main/resources/scripts/import.sql
+++ src/main/resources/scripts/import.sql
@@ -1,2 +1,10 @@
+# arts insertion
 INSERT INTO arts (id, title) VALUES ('1', 'Mona Lisa');
-INSERT INTO arts (id, title) VALUES ('2', 'No Named');
+INSERT INTO arts (id, title) VALUES ('2', 'Saturn Devouring His Son');
+INSERT INTO arts (id, title) VALUES ('3', 'The Starry Night');
+INSERT INTO arts (id, title) VALUES ('4', 'Girl with a Pearl Earring');
+INSERT INTO arts (id, title) VALUES ('5', 'The Night Watch');
+INSERT INTO arts (id, title) VALUES ('6', 'The Persistence of Memory');
+INSERT INTO arts (id, title) VALUES ('7', 'The Son of Man');
+
+
